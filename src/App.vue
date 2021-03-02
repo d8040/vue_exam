a44365a83e254375d3a3a54ebcd218eceb6f092e
d8040 <d8040@nate.com>
2021-03-02 13:21:37 +0900

    태일위드 적용 완료

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate beatae illum eum cum quaerat velit? Eveniet doloribus animi molestiae quasi. Consequatur consectetur, corporis voluptatibus totam iste accusamus nostrum dolor in.
+  <div class="container mx-auto bg-red-500">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum facilis expedita placeat quod quam fuga deserunt excepturi illo dolorem quas dolore ipsa vel odio dicta accusamus, veniam cupiditate ex error.
+    </div>
 </template>
 
 <script lang="ts">
