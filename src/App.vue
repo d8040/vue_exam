d8373e93b1d069071225175c36b37156ccaf0e12
d8040 <d8040@nate.com>
2021-03-02 12:17:50 +0900

    초기 셋팅, App.vue삭제할 항목 삭제

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate beatae illum eum cum quaerat velit? Eveniet doloribus animi molestiae quasi. Consequatur consectetur, corporis voluptatibus totam iste accusamus nostrum dolor in.
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+html, body {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
