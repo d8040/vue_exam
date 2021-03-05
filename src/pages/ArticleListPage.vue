90348bf3679a6a17662f3122e7bcd2e42dfc93bd
d8040 <d8040@nate.com>
2021-03-05 13:35:19 +0900

    뷰라우터로 메인페이지와 게이물리스트 페이지 라우팅

---
+++ src/pages/ArticleListPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container mx-auto">
+    Article List Page
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'AppListPage',
+  components: {
+  }
+})
+</script>
+
+<style>
+</style>
