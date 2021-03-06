f886bc9f7ef7c9d62a7415f0dcf5b8763a6a8414
d8040 <d8040@nate.com>
2021-03-06 16:20:21 +0900

    상황별 버튼 CSS 생성

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -5,13 +5,27 @@
     <div class="container mx-auto">
       <form action="">
         <FormRow title="제목">
-          <input type="text" class="form-row-input" placeholder="제목을 입력해 주세요.">
+          <input
+            type="text"
+            class="form-row-input"
+            placeholder="제목을 입력해 주세요."
+          />
         </FormRow>
         <FormRow title="내용">
-          <textarea class="form-row-input" placeholder="내용을 입력해 주세요."></textarea>
+          <textarea
+            class="form-row-input"
+            placeholder="내용을 입력해 주세요."
+          ></textarea>
         </FormRow>
-        <FormRow>
-          <input type="button" value="작성" class="btn-primary" />
+        <FormRow title="작성">
+          <div class="btns">
+            <input type="button" value="작성" class="btn-primary" />
+            <input type="button" value="작성" class="btn-secondary" />
+            <input type="button" value="작성" class="btn-success" />
+            <input type="button" value="작성" class="btn-danger" />
+            <input type="button" value="작성" class="btn-warning" />
+            <input type="button" value="작성" class="btn-info" />
+          </div>
         </FormRow>
       </form>
     </div>
@@ -19,15 +33,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import TitleBar from '../components/TitleBar.vue'
+import { defineComponent } from "vue";
+import TitleBar from "../components/TitleBar.vue";
 
 export default defineComponent({
   components: { TitleBar },
-  name: 'AppListPage',
-})
+  name: "AppListPage",
+});
 </script>
 
 <style scoped>
-
 </style>
