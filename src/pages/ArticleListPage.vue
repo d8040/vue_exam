1abc9602acde816dbb24648e9065c07462e7e985
d8040 <d8040@nate.com>
2021-03-09 03:32:11 +0900

    전체 디자인 여백 조정, prettier 확장프로그램 적용 후 자동 정렬

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -4,31 +4,20 @@
 	<section class="section section-article-list px-2">
 		<div class="container mx-auto">
 			<div class="mt-6" v-bind:key="article.id" v-for="article in state.articles">
-				<div class="px-10 py-6 bg-white rounded-lg shadow-md">
+				<div class="px-6 py-6 bg-white rounded-lg shadow-md">
 					<div class="flex justify-between items-center">
 						<span class="font-light text-gray-600">{{ article.regDate }}</span>
-						<router-link
-							:to="'list?boardId=' + article.boardId"
-							class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500"
-						>{{ article.boardId == 1 ? "공지" : "자유" }}</router-link>
+						<router-link :to="'list?boardId=' + article.boardId" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">{{ article.boardId == 1 ? "공지" : "자유" }}</router-link>
 					</div>
 					<div class="mt-2">
-						<router-link
-							:to="'detail?id=' + article.id"
-							class="text-2xl text-gray-700 font-bold hover:underline"
-						>{{ article.title }}</router-link>
+						<router-link :to="'detail?id=' + article.id" class="text-2xl text-gray-700 font-bold hover:underline">{{ article.title }}</router-link>
 						<p class="mt-2 text-gray-600">{{ article.body }}</p>
 					</div>
 					<div class="flex justify-between items-center mt-4">
 						<router-link :to="'detail?id=' + article.id" class="text-blue-500 hover:underline">자세히 보기</router-link>
-
 						<div>
 							<router-link :to="'detail?id=' + article.id" class="flex items-center">
-								<img
-									src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
-									alt="avatar"
-									class="mx-4 w-10 h-10 object-cover rounded-full"
-								/>
+								<img src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full" />
 								<h1 class="text-gray-700 font-bold hover:underline">{{ article.extra__writer }}</h1>
 							</router-link>
 						</div>
@@ -40,105 +29,96 @@
 </template>
 
 <script lang="ts">
-import {
-	defineComponent,
-	ref,
-	reactive,
-	getCurrentInstance,
-	onMounted,
-	watch,
-} from "vue";
-import { IArticle } from "../types/";
-import { MainApi } from "../apis/";
-import TitleBar from "../components/TitleBar.vue";
-import axios from "axios";
+	import { defineComponent, ref, reactive, getCurrentInstance, onMounted, watch } from "vue";
+	import { IArticle } from "../types/";
+	import { MainApi } from "../apis/";
+	import TitleBar from "../components/TitleBar.vue";
+	import axios from "axios";
 
-export default defineComponent({
-	name: "AppListPage",
-	props: {
-		boardId: {
-			type: Number,
-			required: true,
-			default: 1,
+	export default defineComponent({
+		name: "AppListPage",
+		props: {
+			boardId: {
+				type: Number,
+				required: true,
+				default: 1,
+			},
 		},
-	},
-	setup(props) {
-		const mainApi: MainApi = getCurrentInstance()?.appContext.config
-			.globalProperties.$mainApi;
+		setup(props) {
+			const mainApi: MainApi = getCurrentInstance()?.appContext.config.globalProperties.$mainApi;
 
-		const newArticleTitleElRef = ref<HTMLInputElement>();
-		const newArticleBodyElRef = ref<HTMLInputElement>();
+			const newArticleTitleElRef = ref<HTMLInputElement>();
+			const newArticleBodyElRef = ref<HTMLInputElement>();
 
-		const state = reactive({
-			articles: [] as IArticle[],
-		});
-
-		function loadArticles(boardId: number) {
-			mainApi.article_list(boardId).then((axiosResponse) => {
-				state.articles = axiosResponse.data.body.articles;
+			const state = reactive({
+				articles: [] as IArticle[],
 			});
-		}
 
-		onMounted(() => {
-			loadArticles(props.boardId);
-		});
+			function loadArticles(boardId: number) {
+				mainApi.article_list(boardId).then((axiosResponse) => {
+					state.articles = axiosResponse.data.body.articles;
+				});
+			}
 
-		watch(
-			() => props.boardId,
-			(newValue, oldValue) => {
+			onMounted(() => {
 				loadArticles(props.boardId);
-			}
-		);
+			});
 
-		function checkAndWriteArticle() {
-			if (newArticleTitleElRef.value == null) {
-				return;
-			}
+			watch(
+				() => props.boardId,
+				(newValue, oldValue) => {
+					loadArticles(props.boardId);
+				}
+			);
 
-			const newArticleTitleEl = newArticleTitleElRef.value;
+			function checkAndWriteArticle() {
+				if (newArticleTitleElRef.value == null) {
+					return;
+				}
 
-			newArticleTitleEl.value = newArticleTitleEl.value.trim();
+				const newArticleTitleEl = newArticleTitleElRef.value;
 
-			if (newArticleTitleEl.value.length == 0) {
-				alert("제목을 입력해 주세요.");
+				newArticleTitleEl.value = newArticleTitleEl.value.trim();
+
+				if (newArticleTitleEl.value.length == 0) {
+					alert("제목을 입력해 주세요.");
+					newArticleTitleEl.focus();
+
+					return;
+				}
+
+				if (newArticleBodyElRef.value == null) {
+					return;
+				}
+
+				const newArticleBodyEl = newArticleBodyElRef.value;
+
+				newArticleBodyEl.value = newArticleBodyEl.value.trim();
+
+				if (newArticleBodyEl.value.length == 0) {
+					alert("내용을 입력해 주세요.");
+					newArticleBodyEl.focus();
+					return;
+				}
+
+				writeArticle(newArticleTitleEl.value, newArticleBodyEl.value);
+
+				newArticleTitleEl.value = "";
+				newArticleBodyEl.value = "";
+
 				newArticleTitleEl.focus();
-
-				return;
 			}
 
-			if (newArticleBodyElRef.value == null) {
-				return;
-			}
+			function writeArticle(title: string, body: string) {}
 
-			const newArticleBodyEl = newArticleBodyElRef.value;
-
-			newArticleBodyEl.value = newArticleBodyEl.value.trim();
-
-			if (newArticleBodyEl.value.length == 0) {
-				alert("내용을 입력해 주세요.");
-				newArticleBodyEl.focus();
-				return;
-			}
-
-			writeArticle(newArticleTitleEl.value, newArticleBodyEl.value);
-
-			newArticleTitleEl.value = "";
-			newArticleBodyEl.value = "";
-
-			newArticleTitleEl.focus();
-		}
-
-		function writeArticle(title: string, body: string) {}
-
-		return {
-			state,
-			checkAndWriteArticle,
-			newArticleTitleElRef,
-			newArticleBodyElRef,
-		};
-	},
-});
+			return {
+				state,
+				checkAndWriteArticle,
+				newArticleTitleElRef,
+				newArticleBodyElRef,
+			};
+		},
+	});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
