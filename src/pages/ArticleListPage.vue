34ae932507a9d3b2d4dffbe38c421641ab5d8908
d8040 <d8040@nate.com>
2021-03-08 23:47:49 +0900

    상단바 반응형 작업

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -7,13 +7,16 @@
 				<div class="px-10 py-6 bg-white rounded-lg shadow-md">
 					<div class="flex justify-between items-center">
 						<span class="font-light text-gray-600">{{ article.regDate }}</span>
-						<a
-							href="#"
+						<router-link
+							:to="'list?boardId=' + article.boardId"
 							class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500"
-						>{{ article.boardId == 1 ? "공지" : "자유" }}</a>
+						>{{ article.boardId == 1 ? "공지" : "자유" }}</router-link>
 					</div>
 					<div class="mt-2">
-						<a href="#" class="text-2xl text-gray-700 font-bold hover:underline">{{ article.title }}</a>
+						<router-link
+							:to="'detail?id=' + article.id"
+							class="text-2xl text-gray-700 font-bold hover:underline"
+						>{{ article.title }}</router-link>
 						<p class="mt-2 text-gray-600">{{ article.body }}</p>
 					</div>
 					<div class="flex justify-between items-center mt-4">
@@ -24,7 +27,7 @@
 								<img
 									src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
 									alt="avatar"
-									class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+									class="mx-4 w-10 h-10 object-cover rounded-full"
 								/>
 								<h1 class="text-gray-700 font-bold hover:underline">{{ article.extra__writer }}</h1>
 							</router-link>
